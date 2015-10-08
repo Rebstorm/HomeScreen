9853640bae14a36b129aed31234f65068b9b5d18
Paul Jacks <jacks@neonred.de>
2015-10-08 10:37:09 +0200

    Initial commit

---
+++ app/src/main/java/jacks/paul/homescreen/fragments/MusicFragment.java
@@ -0,0 +1,19 @@
+package jacks.paul.homescreen.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by Paul on 08/10/2015.
+ */
+public class MusicFragment extends Fragment {
+
+    public MusicFragment(){
+
+
+
+    }
+
+
+
+
+}
